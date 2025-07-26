99b013230deb22c3329be6c8d6d8eb025dcaf01f
Nikita Dhingra <developer.nikita11@gmail.com>
2025-07-26 22:36:21 +0530

    layout added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignIn from './components/signIn/signIn.jsx';
 import AdminDasboard from './components/dashboard/admin/adminDashboard.jsx';
 import EmployeeDashboard from './components/dashboard/employee/employeeDashboard.jsx';
+import Layout from './components/common/layout/layout.jsx';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn/>} />
         <Route path="/signin" element={<SignIn/>} />
-        <Route path="/admin-dashboard" element={<AdminDasboard />} />
+        <Route path="/admin-dashboard" element={<Layout><AdminDasboard /></Layout>} />
         <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
         </Routes>
     </BrowserRouter>
--- frontend/src/api/loginApi.jsx
+++ frontend/src/api/loginApi.jsx
@@ -5,7 +5,6 @@ export const loginAdmin = async (data) => {
     try {
         const response = await axios.post(
             `http://localhost:8080/auth/login-admin`,
-
             {
                 email: data.email,
                 password: data.password,
