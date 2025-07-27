30c9b0b5e4a15495863ec9d26a580df6ec82e3f2
Nikita Dhingra <developer.nikita11@gmail.com>
2025-07-27 23:10:40 +0530

    employee api added

---
+++ frontend/src/api/employeeApi.jsx
@@ -0,0 +1,94 @@
+import axios from 'axios'
+
+// ----------------------GetData-----------------------------------------------
+
+export const getEmployees = async (page, limit,accessToken) => {
+    try {
+        const headers = {
+            'Authorization': `Bearer ${accessToken}`
+        };
+        const response = await axios.get(
+            `http://localhost:8081/api/all-employees?page=${page}&limit=${limit}`,{headers:headers}
+        )
+        // console.log("data", response.data);
+        return response
+    } catch (error) {
+        // console.log("error : ", error);
+        return error.response
+    }
+}
+
+// -----------------------------CreateData---------------------------------------
+
+export const createEmployee = async (form_data,accessToken) => {
+    try {
+        const headers = {
+            'Authorization': `Bearer ${accessToken}`
+        };
+        const response = await axios.post(
+            'http://localhost:8081/api/create-employee',
+            {
+                userType: form_data.userType,
+                name: form_data.name,
+                email: form_data.email,
+                phone: form_data.phone,
+                designation: form_data.designation,
+                department: form_data.department,
+                salary: form_data.salary,
+                date_of_joining: form_data.date_of_joining,
+            },{headers:headers}
+        )
+        // console.log(response);
+        return response
+    } catch (error) {
+        // console.log("error:", error);
+        return error.response
+    }
+}
+
+//--------------------------------DeleteData------------------------------------------
+
+export const deleteEmployee = async (employee_id,accessToken) => {
+    // console.log("authID:", employee_id)
+    try {
+        const headers = {
+            'Authorization': `Bearer ${accessToken}`
+        };
+        const response = await axios.delete(
+            `http://localhost:8081/api/delete-employee?employeeId=${employee_id}`,{headers:headers}
+        )
+        // console.log(response)
+        return response
+    } catch (error) {
+        // console.log("autherror", error)
+        return error.response
+    }
+}
+
+//---------------------------------EditData------------------------------------------
+
+export const editEmployee = async (form_data,accessToken) => {
+    try {
+        const headers = {
+            'Authorization': `Bearer ${accessToken}`
+        };
+        const response = await axios.put(
+            'http://localhost:8081/api/edit-employee',
+            {
+                userType: form_data.userType,
+                employeeId: form_data.id,
+                name: form_data.name,
+                email: form_data.email,
+                phone: form_data.phone,
+                designation: form_data.designation,
+                department: form_data.department,
+                salary: form_data.salary,
+                date_of_joining: form_data.date_of_joining,
+            },{headers:headers}
+        )
+        // console.log(response);
+        return response
+    } catch (error) {
+        return error.response
+    }
+}
--- frontend/src/api/loginApi.jsx
+++ frontend/src/api/loginApi.jsx
@@ -17,3 +17,22 @@ export const loginAdmin = async (data) => {
         return error.response
     }
 }
+
+// ----------------------loginUserAPI-----------------------------------------------
+
+export const loginUser = async (loginData) => {
+    try {
+        const response = await axios.post(
+            `http://localhost:8081/auth/login-employee`,
+            {
+                email: loginData.email,
+                password: loginData.password,
+            }
+        )
+        // console.log("response", response.data);
+        return response
+    } catch (error) {
+        // console.log("error",error)
+        return error.response
+    }
+}
