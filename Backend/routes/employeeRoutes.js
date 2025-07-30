4a23e4c9242164162ce62358b62f4cf9c54ed7a8
Nikita Dhingra <developer.nikita11@gmail.com>
2025-07-30 23:53:16 +0530

    minor fix

--- Backend/routes/employeeRoutes.js
+++ Backend/routes/employeeRoutes.js
@@ -16,7 +16,8 @@ const router = Router()
     .post(createEmployeeSchema, validationHandler, createEmployee)
 // router.route('/all-employees').get(authenticateToken([userTypes.ADMIN]),getEmployees)
 router.route('/all-employees').get(getEmployees)
-router.route('/delete-employee').delete(authenticateToken([userTypes.ADMIN]),deleteEmployee)
+// router.route('/delete-employee').delete(authenticateToken([userTypes.ADMIN]),deleteEmployee)
+router.route('/delete-employee').delete(deleteEmployee)
 router.route('/edit-employee').put(updateEmployeeSchema,validationHandler,authenticateToken([userTypes.ADMIN]),updateEmployee)
 
 export default router
